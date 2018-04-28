1db0f2623ff38a18e77cf1f80119d038e611c354
warrenbrown <warren@katapultmedia.com>
2018-04-28 11:53:43 -0500

    added session tracking

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,9 @@
 exports.render = function(req, res) {
+  if (req.session.lastVisit) {
+    console.log(req.lastVisit);
+  }
+
+  req.session.lastVisit = new Date();
   res.render('index', {
     title: 'Hello Fortnite People'
   })
