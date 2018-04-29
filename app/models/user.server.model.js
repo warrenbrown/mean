9dd652e4666f3830d1188e11f23ba7a66aa027e3
warrenbrown <warren@katapultmedia.com>
2018-04-29 13:32:14 -0500

    added passport dependancy

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,51 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  userName: String,
-  password: String
+  email: {
+    type: String,
+    index: true,
+    required: true,
+    match: /.+\@.+\..+/
+  },
+  userName: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true
+  },
+  website: {
+    type: String,
+    get: function(url) {
+      if (!url) {
+        return url;
+      } else {
+if (url.indexOf('http://') !== 0   && url.indexOf('https://') !== 0) {
+          url = 'http://' + url;
+        }
+
+        return url;
+     }
+    }
+  },
+  password: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+UserSchema.methods.authenticate = function(password) {
+  return this.password === password;
+};
+
+UserSchema.statics.findOneByUsername = function (username, callback) {
+  this.findOne({ username: new RegExp(username, 'i') }, callback);
+};
+
+UserSchema.virtual('fullName').get(function() {
+  return this.firstName + ' ' + this.lastName;
+});
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', UserSchema);
